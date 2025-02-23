d46c7390ff6cbdf94d5e55b1c221e74274e6d235
Ahmed Murshid <ahmed.abdullahione@gmail.com>
2025-02-22 22:41:32 -0500

    DTO added

---
+++ scr/LouCareHack.Backend/LouCareHack.API/Application/DTOs/CaseDTO.cs
@@ -0,0 +1,20 @@
+namespace LouCareHack.API.Application.DTOs;
+
+public partial class CaseDTO
+{
+    public Guid Id { get; set; }
+
+    public int CaseNumber { get; set; }
+
+    public Guid UserId { get; set; }
+
+    public Guid? CaseManagerId { get; set; }
+
+    public Guid CaseStatusId { get; set; }
+
+    public int Priority { get; set; }
+
+    public DateTime CaseDate { get; set; }
+
+    public DateTime CreateAt { get; set; }
+}
